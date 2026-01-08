5be0fa11ac4e3d1e8012d97fb367f51c4e0e61ad
Naitik Pal <naitikpal513@gmail.com>
2026-01-08 18:50:13 +0530

    Add frame extractor file

---
+++ backend/preprocess/frame_extractor.py
@@ -0,0 +1,23 @@
+import cv2
+
+def extract_frames(video_path, fps=1):
+    cap = cv2.VideoCapture(video_path)
+
+    video_fps = cap.get(cv2.CAP_PROP_FPS)
+    interval = max(int(video_fps // fps), 1)
+
+    frames = []
+    idx = 0
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        if idx % interval == 0:
+            frames.append(frame)
+
+        idx += 1
+
+    cap.release()
+    return frames
